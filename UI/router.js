ed80e18643e765cf63c47504de7b02a59eafa714
whiletruedothis <rodrigo.torricodelcastillo@gmail.com>
2019-10-04 09:55:36 -0400

    configured angular

--- UI/router.js
+++ UI/router.js
@@ -3,7 +3,7 @@ const URL = 'http://localhost:3000';
 APP.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl : './views/home.html',
+            templateUrl : './UI/views/home.html',
             controller : 'HomeController'
         })
 
